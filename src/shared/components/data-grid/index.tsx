bb7fcdf1179e62165f086683cf6fccbe087f33c8
NicollasDelgado <nicollasrodriguesdelgado@gmail.com>
2025-07-28 20:59:37 -0300

    dependencias para resolver

--- src/shared/components/data-grid/index.tsx
+++ src/shared/components/data-grid/index.tsx
@@ -1,3 +1,35 @@
-export const TableComponent = () => {
-  return <div>aqui terá a tabela</div>
+import * as React from 'react'
+import {
+  DataGrid,
+  GridColDef,
+  GridRowsProp,
+  GridToolbar,
+} from '@mui/x-data-grid'
+import Box from '@mui/material/Box'
+
+interface CustomDataGridProps {
+  rows: GridRowsProp
+  columns: GridColDef[]
+}
+
+export default function CustomDataGrid({ rows, columns }: CustomDataGridProps) {
+  return (
+    <Box sx={{ height: 400, width: '100%' }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        pageSizeOptions={[5, 10, 20]} // Adicionei mais opções de tamanho de página
+        checkboxSelection
+        disableRowSelectionOnClick
+        slots={{ toolbar: GridToolbar }} // Adiciona a barra de ferramentas
+      />
+    </Box>
+  )
 }
