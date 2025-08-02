0e8e0899c23e6b219039e898134e60a6929a7b84
Rodrigo Bighetti <robighetti@gmail.com>
2025-08-02 08:42:52 -0300

    ajustes no build

--- src/routes/OpenRoutes.tsx
+++ src/routes/OpenRoutes.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { Login, ForgotPassword, ResetPassword, SignUp } from '../pages'
+import { Login, ForgotPassword, ResetPassword } from '../pages'
 
 export const OpenRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/sign-up" element={<SignUp />} />
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/reset-password/:token" element={<ResetPassword />} />
       <Route path="*" element={<Navigate to="/" />} />
